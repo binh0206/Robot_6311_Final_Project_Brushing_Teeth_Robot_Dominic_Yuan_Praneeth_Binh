d92e41c2d388289a4fa55c18b18b4f1a09a28e86
binh0206 <43211598+binh0206@users.noreply.github.com>
2018-12-02 19:46:06 -0600

    Add files via upload

---
+++ Kinect2FaceHD_NET/MainWindow.xaml.cs
@@ -0,0 +1,180 @@
+﻿
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Face;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.IO.Ports;
+using System.IO;
+
+
+
+namespace Kinect2FaceHD_NET
+{
+    public partial class MainWindow : Window
+    {
+        private KinectSensor _sensor = null;
+
+        private BodyFrameSource _bodySource = null;
+
+        private BodyFrameReader _bodyReader = null;
+
+        private HighDefinitionFaceFrameSource _faceSource = null;
+
+        private HighDefinitionFaceFrameReader _faceReader = null;
+
+        private FaceAlignment _faceAlignment = null;
+
+        private FaceModel _faceModel = null;
+
+        private List<Ellipse> _points = new List<Ellipse>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _sensor = KinectSensor.GetDefault();
+
+            if (_sensor != null)
+            {
+                _bodySource = _sensor.BodyFrameSource;
+                _bodyReader = _bodySource.OpenReader();
+                _bodyReader.FrameArrived += BodyReader_FrameArrived;
+
+                _faceSource = new HighDefinitionFaceFrameSource(_sensor);
+
+                _faceReader = _faceSource.OpenReader();
+                _faceReader.FrameArrived += FaceReader_FrameArrived;
+
+                _faceModel = new FaceModel();
+                _faceAlignment = new FaceAlignment();
+
+                _sensor.Open();
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (_faceModel != null)
+            {
+                _faceModel.Dispose();
+                _faceModel = null;
+            }
+
+            GC.SuppressFinalize(this);
+        }
+
+        private void BodyReader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            using (var frame = e.FrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    Body[] bodies = new Body[frame.BodyCount];
+                    frame.GetAndRefreshBodyData(bodies);
+
+                    Body body = bodies.Where(b => b.IsTracked).FirstOrDefault();
+
+                    if (!_faceSource.IsTrackingIdValid)
+                    {
+                        if (body != null)
+                        {
+                            _faceSource.TrackingId = body.TrackingId;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void FaceReader_FrameArrived(object sender, HighDefinitionFaceFrameArrivedEventArgs e)
+        {
+            using (var frame = e.FrameReference.AcquireFrame())
+            {
+                if (frame != null && frame.IsFaceTracked)
+                {
+                    frame.GetAndRefreshFaceAlignmentResult(_faceAlignment);
+                    UpdateFacePoints();
+                }
+            }
+        }
+
+        private void UpdateFacePoints()
+        {
+            int wait = 0;
+            int step = 1;
+            if (_faceModel == null) return;
+
+            var vertices = _faceModel.CalculateVerticesForAlignment(_faceAlignment);
+
+            if (vertices.Count > 0)
+            {
+                if (_points.Count == 0)
+                {
+                    for (int index = 0; index < vertices.Count; index++)
+                    {
+                        Ellipse ellipse = new Ellipse
+                        {
+                            Width = 2.0,
+                            Height = 2.0,
+                            Fill = new SolidColorBrush(Colors.Blue)
+                        };
+
+                        _points.Add(ellipse);
+                    }
+
+                    foreach (Ellipse ellipse in _points)
+                    {
+                        canvas.Children.Add(ellipse);
+                    }
+                }
+
+                for (int index = 0; index < vertices.Count; index++)
+                {
+
+                    wait++;
+
+                    CameraSpacePoint vertice = vertices[index];
+                    DepthSpacePoint point = _sensor.CoordinateMapper.MapCameraPointToDepthSpace(vertice);
+                    CameraSpacePoint vertice_mouth_corner = vertices[91];
+                    float a = vertice_mouth_corner.X;
+                    float b = vertice_mouth_corner.Y;
+                    float c = vertice_mouth_corner.Z;
+                    //Console.WriteLine("wait: {0} ", wait);
+                    //Console.WriteLine("step: {0} ", step);
+                    if (wait >= step * 1345)
+                    {
+                        step++;
+
+                        Console.WriteLine("X coordinates: {0} ", a);
+                        Console.WriteLine("         Y coordinates: {0} ", b);
+                        Console.WriteLine("                 Z coordinates: {0} ", c);
+                        //System.Threading.Thread.Sleep(100);
+                        StreamWriter fout = new StreamWriter("C:/Fall 2018 textbook/Robotics/use/kinect-2-face-hd-master/Kinect2FaceHD/Kinect2FaceHD_NET/bin/x64/Debug/name.txt");
+                        fout.WriteLine(a);
+                        fout.WriteLine(b);
+                        fout.WriteLine(c);
+                        fout.Close();
+
+                    }
+                    if (float.IsInfinity(point.X) || float.IsInfinity(point.Y)) return;
+
+                    Ellipse ellipse = _points[index];
+
+                    Canvas.SetLeft(ellipse, point.X);
+                    Canvas.SetTop(ellipse, point.Y);
+
+
+                }
+            }
+        }
+    }
+}
