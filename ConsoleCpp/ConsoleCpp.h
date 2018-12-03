4d5f9b875cd39441403a07179c6c4a77b677c63d
binh0206 <43211598+binh0206@users.noreply.github.com>
2018-12-02 19:33:37 -0600

    Add files via upload

---
+++ ConsoleCpp/ConsoleCpp.h
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+//#include <thread>         // std::this_thread::sleep_for
+#include <chrono>         // std::chrono::seconds
+#include <windows.h>
+#include <string.h>
+#include <string>
+
+
+
+
+using namespace System;
+using namespace System::IO::Ports;
